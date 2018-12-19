b80cbbbe06a0b0061fc2fb5abd7c20bda780a823
Jonas Klappert <klappert@physik.rwth-aachen.de>
2018-12-19 10:35:03 +0100

    Introduce 128 bit prime numbers for future setup.

--- source/include/ReconstHelper.hpp
+++ source/include/ReconstHelper.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <vector>
+#include <string>
 
 namespace firefly {
 
@@ -9,4 +10,9 @@ namespace firefly {
    *    A collection of 63 bit prime numbers
    */
   const std::vector<uint64_t>& primes();
+  
+  /**
+   * 
+   */
+  const std::vector<std::string>& primes_128();
 }
