2fc5ef763148a84d9a8dfe2ab7bfff850ce3a787
Jonas Klappert <klappert@physik.rwth-aachen.de>
2018-12-19 10:34:20 +0100

    Change the hasher for pairs.

--- source/include/UintHasher.hpp
+++ source/include/UintHasher.hpp
@@ -19,12 +19,7 @@ namespace firefly {
   class UintPairHasher {
   public:
     std::size_t operator()(std::pair<uint, uint> const& pair) const {
-      std::size_t seed = 2;
-
-      seed ^= pair.first + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-      seed ^= pair.second + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-
-      return seed;
+      return std::hash<uint>()(pair.first) ^ std::hash<uint>()(pair.second);
     }
   };
 }
