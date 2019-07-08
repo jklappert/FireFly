66350fbf8802304c3efaacdeb4f06561165f779c
Jonas Klappert <klappert@physik.rwth-aachen.de>
2019-07-08 13:49:20 +0200

    remove backslash.

--- mma_2_ff/convert_to_ff.m
+++ mma_2_ff/convert_to_ff.m
@@ -33,7 +33,7 @@ convert[funs_, vars_, nthr_] :=
 	   WriteString[stream2, "  return 0;\n}\n\n"];
 	   WriteString[stream2, "class BlackBoxUser : public BlackBoxBase{\npublic:\nBlackBoxUser(){};\n"];
 	   WriteString[stream2, "  virtual std::vector<FFInt> operator()(const std::vector<FFInt>& values){\n  }\n"];
-	   WriteString[stream2, "  virtual void prime_changed(){\n  }\n}\;"];
+	   WriteString[stream2, "  virtual void prime_changed(){\n  }\n};\n"];
 	   Close[stream2];
 	   
 	   stream = OpenWrite["ff_conv/funs.hpp"];
