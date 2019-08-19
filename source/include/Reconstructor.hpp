e93712b610235be0c9161fa69453829361422f2b
Jonas Klappert <klappert@physik.rwth-aachen.de>
2019-08-19 16:05:14 +0200

    Change type of clock.

--- source/include/Reconstructor.hpp
+++ source/include/Reconstructor.hpp
@@ -140,8 +140,8 @@ namespace firefly {
     enum verbosity_levels {SILENT, IMPORTANT, CHATTY};
     enum RatReconst_status {RECONSTRUCTING, DONE, DELETED};
   private:
-    std::chrono::_V2::system_clock::time_point start = std::chrono::high_resolution_clock::now();
-    std::chrono::_V2::system_clock::time_point prime_start = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point prime_start = std::chrono::high_resolution_clock::now();
     uint32_t n;
     uint32_t thr_n;
     uint32_t bunch_size = 1;
