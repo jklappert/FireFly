1f7f48fd45cefcadd3fce437846ad8fca51def1f
Jonas Klappert <klappert@physik.rwth-aachen.de>
2020-03-26 12:03:00 +0100

    Add the option to insert several expressions located in a directory.

--- ff_insert.cpp
+++ ff_insert.cpp
@@ -18,6 +18,7 @@
 
 #include "Reconstructor.hpp"
 #include "AmplitudeParser.hpp"
+#include "tinydir.h"
 #include <sys/stat.h>
 
 using namespace firefly;
@@ -154,175 +155,224 @@ int main(int argc, char* argv[]) {
   }
 
   logger.close();
-  // Construct the amplitude parser
-  AmplitudeParser ap(vars, families);
 
-  // Parse the amplitude
-  ap.parse_amplitude_file(input_file);
+  std::vector<std::string> expression_paths;
+  std::vector<std::string> expression_files;
+  std::unordered_map<std::string, std::string> paths_map;
+  tinydir_dir expression_dir;
+  tinydir_open_sorted(&expression_dir, input_file.c_str());
 
-  // Parse IBP tables
-  tinydir_dir dir;
-  tinydir_open_sorted(&dir, "replacements");
+  if (expression_dir.n_files != 0) {
+    if (input_file[input_file.size() - 1] == '/')
+      input_file.erase(input_file.size() - 1 , 1);
 
-  std::vector<std::string> files;
+    for (size_t i = 0; i != expression_dir.n_files; ++i) {
+      tinydir_file file;
+      tinydir_readfile_n(&expression_dir, &file, i);
 
-  for (size_t i = 0; i != dir.n_files; ++i) {
-    tinydir_file file;
-    tinydir_readfile_n(&dir, &file, i);
+      if (!file.is_dir) {
+        expression_paths.emplace_back(input_file + "/" + file.name);
+	expression_files.emplace_back(file.name);
+      }
+    }
 
-    if (!file.is_dir) {
-      files.emplace_back(file.name);
+    tinydir_close(&expression_dir);
+  } else {
+    expression_paths.emplace_back(input_file);
+    size_t tmp_pos = input_file.rfind('/');
+
+    if (tmp_pos != 0) {
+      expression_files.emplace_back(input_file.substr(tmp_pos + 1, input_file.size() - 1 - tmp_pos));
+    } else {
+      expression_files.emplace_back(input_file);
     }
   }
 
-  tinydir_close(&dir);
+  tinydir_close(&expression_dir);
 
-  for (const auto& file : files) {
-    ap.parse_ibp_table_file("replacements/" + file);
-  }
+  for (size_t j = 0; j != expression_paths.size(); ++j) {
+    std::cout << expression_paths.size() << " " << expression_files.size() << "\n";
+    std::string expression = expression_paths[j];
+    std::string fn = expression_files[j];
+    // Construct the amplitude parser
+    AmplitudeParser ap(vars, families);
 
-  // Build the black boxes and start reconstruction
-  size_t masters = ap.check_for_unreplaced_masters();
+    // Parse the amplitude
+    ap.parse_amplitude_file(expression);
 
-  std::ofstream basis_f_file;
-  basis_f_file.open("basis_functions");
-  basis_f_file.close();
+    // Parse IBP tables
+    tinydir_dir dir;
+    tinydir_open_sorted(&dir, "replacements");
 
-  if (!no_interpolation) {
-    std::ofstream file;
-    file.open("out.m");
-    file << "{\n";
-    file.close();
+    std::vector<std::string> files;
 
-    for (size_t i = 0; i != masters; ++i) {
-      basis_f_file.open("basis_functions", std::ios_base::app);
-      basis_f_file << ap.get_master(i) << "\n";
-      basis_f_file.close();
+    for (size_t i = 0; i != dir.n_files; ++i) {
+      tinydir_file file;
+      tinydir_readfile_n(&dir, &file, i);
 
-      if (skip_functions.find(ap.get_master(i)) != skip_functions.end()) {
-        INFO_MSG("Skipping basis function: " + ap.get_master(i));
-        logger.open("ff_insert.log", std::ios_base::app);
-        logger << "Skipping basis function: " + ap.get_master(i) + "\n\n";
-        logger.close();
-      } else {
-        if (i == 0) {
-          INFO_MSG("Reconstructing coefficient of basis function: " + ap.get_master(i) + "\n");
+      if (!file.is_dir) {
+        files.emplace_back(file.name);
+      }
+    }
+
+    tinydir_close(&dir);
+
+    for (const auto& file : files) {
+      ap.parse_ibp_table_file("replacements/" + file);
+    }
+
+    // Build the black boxes and start reconstruction
+    size_t masters = ap.check_for_unreplaced_masters();
+
+    std::ofstream basis_f_file;
+    basis_f_file.open("basis_functions");
+    basis_f_file.close();
+
+    if (!no_interpolation) {
+      std::ofstream file;
+      std::string file_name = "out_" + fn;
+      file.open(file_name.c_str());
+      file << "{\n";
+      file.close();
+
+      for (size_t i = 0; i != masters; ++i) {
+        basis_f_file.open("basis_functions", std::ios_base::app);
+        basis_f_file << ap.get_master(i) << "\n";
+        basis_f_file.close();
+
+        if (skip_functions.find(ap.get_master(i)) != skip_functions.end()) {
+          INFO_MSG("Skipping basis function: " + ap.get_master(i));
           logger.open("ff_insert.log", std::ios_base::app);
-          logger << "Reconstructing coefficient of basis function: " + ap.get_master(i) + "\n";
+          logger << "Skipping basis function: " + ap.get_master(i) + "\n\n";
           logger.close();
-        }
-
-        auto bb = ap.build_black_box(i);
-
-        // Construct the reconstructor
-        Reconstructor<FFAmplitudeBlackBox> reconst(bb.n, n_threads, bs, bb);
-
-        // Settings
-        if (factor_scan)
-          reconst.enable_factor_scan();
-
-        reconst.enable_shift_scan();
-
-        bool renamed_ff_save = false;
-
-        if (save_mode) {
-          std::string tmp = "ff_save_" + ap.get_master(i);
-          struct stat buffer;
-
-          if (stat(tmp.c_str(), &buffer) == 0) {
-            struct stat buffer_2;
-
-            if (stat("ff_save", &buffer_2) == 0) {
-              std::rename("ff_save", "ff_save_tmp");
-              renamed_ff_save = true;
-            }
-
-            std::rename(tmp.c_str(), "ff_save");
+        } else {
+          if (i == 0) {
+            INFO_MSG("Reconstructing coefficient of basis function: " + ap.get_master(i) + "\n");
+            logger.open("ff_insert.log", std::ios_base::app);
+            logger << "Reconstructing coefficient of basis function: " + ap.get_master(i) + "\n";
+            logger.close();
           }
 
-          reconst.set_tags({ap.get_master(i)});
-          reconst.resume_from_saved_state();
+          auto bb = ap.build_black_box(i);
+
+          // Construct the reconstructor
+          Reconstructor<FFAmplitudeBlackBox> reconst(bb.n, n_threads, bs, bb);
+
+          // Settings
+          if (factor_scan)
+            reconst.enable_factor_scan();
+
+          reconst.enable_shift_scan();
+
+          bool renamed_ff_save = false;
+
+          if (save_mode) {
+            std::string tmp = "ff_save_" + ap.get_master(i);
+            struct stat buffer;
+
+            if (stat(tmp.c_str(), &buffer) == 0) {
+              struct stat buffer_2;
+
+              if (stat("ff_save", &buffer_2) == 0) {
+                std::rename("ff_save", "ff_save_tmp");
+                renamed_ff_save = true;
+              }
+
+              std::rename(tmp.c_str(), "ff_save");
+            }
+
+            reconst.set_tags({ap.get_master(i)});
+            reconst.resume_from_saved_state();
+          }
+
+          // Reconstruct
+          reconst.reconstruct();
+
+          std::vector<RationalFunction> results = reconst.get_result();
+          file.open(file_name.c_str(), std::ios_base::app);
+
+          if (!results.back().zero())
+            file <<  "+ " << ap.get_master(i) << "*" + results.back().generate_horner(vars) << "\n";
+
+          file.close();
+
+          if (save_mode) {
+            std::string tmp = "ff_save_" + ap.get_master(i);
+            std::rename("ff_save", tmp.c_str());
+
+            if (renamed_ff_save)
+              std::rename("ff_save_tmp", "ff_save");
+          }
+
+          RatReconst::reset();
+
+          std::ifstream in("firefly.log");
+
+          if (in.good()) {
+            std::string old_log((std::istreambuf_iterator<char>(in)),
+                                (std::istreambuf_iterator<char>()));
+            logger.open("ff_insert.log", std::ios_base::app);
+            logger << old_log << "\n";
+            logger << "-------------------------------------------------------------\n\n";
+            logger.close();
+          }
         }
 
-        // Reconstruct
-        reconst.reconstruct();
-
-        std::vector<RationalFunction> results = reconst.get_result();
-        file.open("out.m", std::ios_base::app);
-
-        if (!results.back().zero())
-          file <<  "+ " << ap.get_master(i) << "*" + results.back().generate_horner(vars) << "\n";
-
-        file.close();
-
-        if (save_mode) {
-          std::string tmp = "ff_save_" + ap.get_master(i);
-          std::rename("ff_save", tmp.c_str());
-
-          if (renamed_ff_save)
-            std::rename("ff_save_tmp", "ff_save");
-        }
-
-        RatReconst::reset();
-
-        std::ifstream in("firefly.log");
-
-        if (in.good()) {
-          std::string old_log((std::istreambuf_iterator<char>(in)),
-                              (std::istreambuf_iterator<char>()));
+        if (i + 1 != masters) {
+          std::cout << "\n";
+          INFO_MSG("Coefficients done: " + std::to_string(i + 1) + " / " + std::to_string(masters) + "\n");
+          INFO_MSG("Reconstructing coefficient of basis function: " + ap.get_master(i + 1) + "\n");
           logger.open("ff_insert.log", std::ios_base::app);
-          logger << old_log << "\n";
+          logger << "Coefficients done: " + std::to_string(i + 1) + " / " + std::to_string(masters) + "\n\n";
           logger << "-------------------------------------------------------------\n\n";
+          logger << "Reconstructing coefficient of basis function: " + ap.get_master(i + 1) + "\n";
           logger.close();
         }
+
       }
 
-      if (i + 1 != masters) {
-        std::cout << "\n";
-        INFO_MSG("Coefficients done: " + std::to_string(i + 1) + " / " + std::to_string(masters) + "\n");
-        INFO_MSG("Reconstructing coefficient of basis function: " + ap.get_master(i + 1) + "\n");
-        logger.open("ff_insert.log", std::ios_base::app);
-        logger << "Coefficients done: " + std::to_string(i + 1) + " / " + std::to_string(masters) + "\n\n";
-        logger << "-------------------------------------------------------------\n\n";
-        logger << "Reconstructing coefficient of basis function: " + ap.get_master(i + 1) + "\n";
-        logger.close();
+      file.open(file_name.c_str(), std::ios_base::app);
+      file << "}\n";
+      file.close();
+
+      std::cout << "\n";
+
+      auto time1 = std::chrono::high_resolution_clock::now();
+      INFO_MSG("Reconstructed expression in " + std::to_string(std::chrono::duration<double>(time1 - time0).count()) + " s");
+      INFO_MSG("Result has been written to '" + file_name + "'");
+      logger.open("ff_insert.log", std::ios_base::app);
+      logger << "Coefficients done: " + std::to_string(masters) + " / " + std::to_string(masters) + "\n\n";
+      logger << "-------------------------------------------------------------\n\n";
+      logger << "Reconstructed expression in " + std::to_string(std::chrono::duration<double>(time1 - time0).count()) + " s\n";
+      logger << "Result has been written to '" << file_name << "'\n";
+      logger.close();
+      std::string new_log_name = "ff_insert_" + fn + ".log";
+      std::rename("ff_insert.log", new_log_name.c_str());
+      std::string new_basis_functions_name = "basis_functions_" + fn;
+      std::rename("basis_functions", new_basis_functions_name.c_str());
+      std::rename("ff_insert.log", new_log_name.c_str());
+      std::remove("firefly.log");
+    } else {
+      mkdir("coefficients", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+      for (size_t i = 0; i != masters; ++i) {
+        basis_f_file.open("basis_functions", std::ios_base::app);
+        basis_f_file << ap.get_master(i) << "\n";
+        basis_f_file.close();
+        std::ofstream coef_file;
+        std::string file_name = "coefficients/" + ap.get_master(i) + ".m";
+        coef_file.open(file_name.c_str());
+        coef_file << "{\n" << ap.get_master(i) + "*(" << ap.get_unsimplified_coef(i) << ")\n}\n";
+        coef_file.close();
       }
 
+      INFO_MSG("Unsimplified coefficients written to 'coefficients' directory");
+      logger.open("ff_insert.log", std::ios_base::app);
+      logger << "Unsimplified coefficients written to 'coefficients' directory\n";
+      logger.close();
+      std::string new_log_name = "ff_insert_" + fn + ".log";
+      std::rename("ff_insert.log", new_log_name.c_str());
     }
-
-    file.open("out.m", std::ios_base::app);
-    file << "}\n";
-    file.close();
-
-    std::cout << "\n";
-
-    auto time1 = std::chrono::high_resolution_clock::now();
-    INFO_MSG("Reconstructed expression in " + std::to_string(std::chrono::duration<double>(time1 - time0).count()) + " s");
-    INFO_MSG("Result has been written to 'out.m'");
-    logger.open("ff_insert.log", std::ios_base::app);
-    logger << "Coefficients done: " + std::to_string(masters) + " / " + std::to_string(masters) + "\n\n";
-    logger << "-------------------------------------------------------------\n\n";
-    logger << "Reconstructed expression in " + std::to_string(std::chrono::duration<double>(time1 - time0).count()) + " s\n";
-    logger << "Result has been written to 'out.m'\n";
-    logger.close();
-  } else {
-    mkdir("coefficients", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-
-    for (size_t i = 0; i != masters; ++i) {
-      basis_f_file.open("basis_functions", std::ios_base::app);
-      basis_f_file << ap.get_master(i) << "\n";
-      basis_f_file.close();
-      std::ofstream coef_file;
-      std::string file_name = "coefficients/" + ap.get_master(i) + ".m";
-      coef_file.open(file_name.c_str());
-      coef_file << "{\n" << ap.get_master(i) + "*(" << ap.get_unsimplified_coef(i) << ")\n}\n";
-      coef_file.close();
-    }
-
-    INFO_MSG("Unsimplified coefficients written to 'coefficients' directory");
-    logger.open("ff_insert.log", std::ios_base::app);
-    logger << "Unsimplified coefficients written to 'coefficients' directory\n";
-    logger.close();
   }
 
   return 0;
